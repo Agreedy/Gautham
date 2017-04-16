3ad2f5d839f0079dd8e1e8cef9932799a25171f8
goutamreddy <goutamreddy@192.168.2.1>
2017-04-15 21:25:06 -0400

    Update

---
+++ My first java project/src/number1.java
@@ -0,0 +1,13 @@
+
+public class number1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Gautham");
+		System.out.println("venkat reddy");
+		System.out.println("padma");
+		System.out.print("shravani");
+		System.out.println("Jeevan");
+	}
+
+}
