6f945377bee6b696482b3316370cb9ffde7429ba
goutamreddy <goutamreddy@192.168.2.1>
2017-04-22 20:00:40 -0400

    Update

---
+++ test 1/src/test1.java
@@ -0,0 +1,15 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+ int oneday = 50;
+int twoday = 100;
+int thirdday = 150;
+
+int total = (oneday + twoday + thirdday);
+
+System.out.println( total);
+
+	}
+
+}
