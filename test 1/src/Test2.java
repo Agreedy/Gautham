74e2b8af296beb223cf0d448ab802d12cdb18f6f
goutamreddy <goutamreddy@192.168.2.1>
2017-04-24 14:58:27 -0400

    update

---
+++ test 1/src/Test2.java
@@ -0,0 +1,29 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+int myage = 20;
+int dadage = 30;
+ int totalage = myage + dadage ;
+	 
+	 System.out.println(totalage);
+	 //* adding mother age 
+	 
+	int motherage = 28;
+	double height = 5.4;
+	double motherh =  height;
+	
+	System.out.println(motherh);
+	System.out.println(totalage + myage);
+	
+	boolean brown = true;
+	char what = 'w' ;
+	String amma = "PADMA";
+	
+	System.out.println("amma color  " +   (amma + brown));
+	
+			
+	}
+
+}
