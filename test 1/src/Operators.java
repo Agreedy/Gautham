1732e5329016e7cac4b7fd477715fd0c1555c3f4
goutamreddy <goutamreddy@192.168.2.1>
2017-04-24 17:57:48 -0400

    update

---
+++ test 1/src/Operators.java
@@ -0,0 +1,74 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+
+		int value = 10;
+		
+		
+//		System.out.println("nextvalue " + value++);
+//		System.out.println("nextvalue " + value++);
+//		System.out.println("nextvalue " + value++);
+//		System.out.println("nextvalue " + value++);
+//		System.out.println("nextvalue ");
+
+		// additional assignments
+		
+//		System.out.println("nextvalue " + value);
+//		value = value+2;
+//		System.out.println("nextvalue " + value);
+//		value = value+2;
+//		value = value+2;
+//
+//		System.out.println("nextvalue " + value);
+//		value = value+2;
+//		System.out.println("nextvalue " + value); //
+	
+ // next operator	
+		
+
+	
+//		System.out.println("nextvalue " + value);
+//		value = value-2;
+//		System.out.println("nextvalue " + value);
+//		value = value-2;
+//		System.out.println("nextvalue " + value);
+//		value = value+2;
+//		System.out.println("nextvalue " + value);
+		
+//		System.out.println("nextvalue " + value);
+//		value = value*2;
+//		System.out.println("nextvalue " + value);
+//		value = value*2;
+//		System.out.println("nextvalue " + value);
+//		value = value*2;
+//		System.out.println("nextvalue " + value);
+		
+		System.out.println("nextvalue " + value);
+		value = value%100;
+		System.out.println("nextvalue " + (value));
+		value = value%1;
+		System.out.println("nextvalue " + value);
+		value = value%700;
+		System.out.println("nextvalue " + value);
+		
+
+		
+		
+
+		
+		
+		
+
+
+
+
+		
+		
+		
+
+		
+
+	}
+
+}
