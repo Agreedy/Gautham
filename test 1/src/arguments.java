5b97c80a9afb113aee98545ccd10252f467ddf47
goutamreddy <goutamreddy@192.168.2.1>
2017-04-29 15:17:46 -0400

    Update

---
+++ test 1/src/arguments.java
@@ -0,0 +1,16 @@
+
+public class arguments {
+
+	public static void main(String[] args) {
+
+		int args1 = Integer.parseInt(args[0]);
+		int args2 = Integer.parseInt(args[1]);
+
+		boolean Value = args1 >= args2;
+		
+		System.out.println(Value);
+		
+		
+	}
+
+}
